5d62e9c4bc1bc1e462133b589a9833f52ec3c167
devendraghormare1 <devendrag@cybage.com>
2025-05-30 11:06:43 +0530

    Repo cleanup

--- README.md
+++
@@ -1,4 +0,0 @@
-create .env file and add the following api keys
-
-GROQ_API_KEY = "Your API Key"
-TOGETHER_API_KEY = "Your API Key"
--- movie_recommender/src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
-EMBEDDING_MODEL = "BAAI/bge-base-en-v1.5"
-LLM_MODEL = "meta-llama/Llama-3-8b-chat-hf"
